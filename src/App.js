aa014b82b46120f8d11680a339f15be6445251a1
ryanyuri357 <ryan.yuri357@gmail.com>
2021-08-02 22:30:33 -0500

    first start

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from "react";
+
+import {Container, Row, Col} from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
+import Axios from "axios";
+
+
 function App() {
+  const [details, setDetails] = useState({})
+
+  const fetchDetails = async () => {
+    const response = await Axios.get("https://randomuser.me/api/")
+    console.log("RESPONSE: ", response)
+  }
+
+  useEffect(() => {
+    fetchDetails()
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
